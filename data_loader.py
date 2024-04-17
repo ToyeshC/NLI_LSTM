354925ae786b853c61e793161f5695c8ee3d7468
ToyeshC <toyesh.c@gmail.com>
2024-04-17 18:27:55 +0200

    updated data loader

--- data_loader.py
+++ data_loader.py
@@ -1,36 +1,76 @@
-# importing libraries
+import torch
+from torch.utils.data import Dataset, DataLoader
 from datasets import load_dataset
-import nltk
-from nltk.tokenize import word_tokenize
-nltk.download('punkt')
-from torchtext.vocab import GloVe
-
-# load dataset from hugging face
-dataset = load_dataset('https://huggingface.co/datasets/stanfordnlp/snli')
-
-# access the train, validation, and test splits
-# train_data = dataset['train']
-# val_data = dataset['validation']
-# test_data = dataset['test']
-
-# pre process the data set (tokenisation and lowercase)
-def preprocess(text):
-    # Tokenize the text using nltk word_tokenize
-    tokens = word_tokenize(text)
-    # Convert tokens to lowercase
-    tokens_lower = [token.lower() for token in tokens]
-    # Join tokens back into a single string
-    processed_text = ' '.join(tokens_lower)
-    return processed_text
-
-# for split in ['train', 'validation', 'test']:
-for split in ['validation']:
-    for i in range(len(dataset[split])):
-        premise = dataset[split][i]['premise']
-        hypothesis = dataset[split][i]['hypothesis']
-        dataset[split][i]['premise'] = preprocess(premise)
-        dataset[split][i]['hypothesis'] = preprocess(hypothesis)
+import numpy as np
+import os
 
 
-# load the GloVe embeddings
-glove_vectors = GloVe(name='840B', dim=300)
+class DatasetLoader(Dataset):
+    def __init__(self, dataset, word_to_vec, embedding_dim=300):
+        self.dataset = dataset
+        self.word_to_vec = word_to_vec
+        self.embedding_dim = embedding_dim
+
+    def __len__(self):
+        return len(self.data['premise'])
+
+    def __getitem__(self, idx):
+        premise = self.data['premise'][idx]
+        hypothesis = self.data['hypothesis'][idx]
+        label = self.data['label'][idx]
+        premise_index, premise_length = self.sentence_indexer(premise)
+        hypothesis_index, hypothesis_length = self.sentence_indexer(hypothesis)
+
+        return premise_index, premise_length, hypothesis_index, hypothesis_length, label
+
+    def sentence_indexer(self, sentence):
+        tokens = sentence.split()
+        index = torch.tensor([self.word_to_vec.get(word, [0]*self.embedding_dim) for word in tokens], dtype=torch.float32)
+
+        length = len(index)
+
+        return index, length
+
+
+def get_vocab(sentences):
+    # create vocab of words
+    vocabulary_dict = {}
+    for sent in sentences:
+        for word in sent.split():
+            if word not in vocabulary_dict:
+                vocabulary_dict[word] = ''
+    vocabulary_dict['<s>'] = ''
+    vocabulary_dict['</s>'] = ''
+    vocabulary_dict['<p>'] = ''
+    return vocabulary_dict
+
+
+def get_glove_embeddings(vocabulary_dict, glove_path):
+    word_to_vec = {}
+
+    with open(glove_path, encoding='utf-8') as file:
+        for line in file:
+            word, vec = line.split(' ', 1)
+            if word in vocabulary_dict:
+                word_to_vec[word] = list(map(float, vec.split()))
+
+    return word_to_vec
+
+
+
+def build_vocab(sentences, glove_path):
+    vocabulary_dict = get_vocab(sentences)
+    word_to_vec = get_glove_embeddings(vocabulary_dict, glove_path)
+    return word_to_vec
+
+
+def collate_fn(batch):
+    premise, s1_lengths, hypothesis, s2_lengths, labels = zip(*batch)
+    
+    premise = torch.nn.utils.rnn.pad_sequence(premise, batch_first=True, padding_value=0)
+    hypothesis = torch.nn.utils.rnn.pad_sequence(hypothesis, batch_first=True, padding_value=0)
+    s1_lengths = torch.tensor(s1_lengths)
+    s2_lengths = torch.tensor(s2_lengths)
+    labels = torch.tensor(labels, dtype=torch.long)
+
+    return premise, s1_lengths, hypothesis, s2_lengths, labels
